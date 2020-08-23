72c44b964fd28fe1f9ad042f1a4c59e4ad4f5126
Marc Dominik Migge <marcmigge@gmx.net>
2020-08-23 14:49:44 +0200

    Finish logo generator

--- _logo.rb
+++ _logo.rb
@@ -5,7 +5,7 @@ inner_triangle = []
 outer_triangle = []
 
 $target_height = 100
-$stroke_width = 5
+$stroke_width = 8
 
 distance_base_to_center = Math::tan(30 * Math::PI / 180) * 0.5
 inner_triangle << Point.new(center.x - 0.5, center.y + distance_base_to_center)
@@ -31,13 +31,13 @@ puts <<~LOGO
 <?xml version="1.0" standalone="no"?>
 <svg width="#{$target_height}" height="#{$target_height}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <symbol id="logo" width="#{$target_height}" height="#{$target_height}">
-        <polygon points="#{inner_triangle_coordinates}" stroke="black" fill="transparent" stroke-width="#{$stroke_width}"/>
-        <circle cx="#{transform(center.x)}" cy="#{transform(center.y)}" r="#{$pixels_per_unit.round}" stroke="black" fill="transparent" stroke-width="#{$stroke_width}"/>
-        <line x1="#{transform(inner_triangle[0].x)}" y1="#{transform(inner_triangle[0].y)}" x2="#{transform(outer_triangle[0].x)}" y2="#{transform(outer_triangle[0].y)}" stroke="black" fill="transparent" stroke-width="#{$stroke_width}"/>
-        <line x1="#{transform(inner_triangle[1].x)}" y1="#{transform(inner_triangle[1].y)}" x2="#{transform(outer_triangle[1].x)}" y2="#{transform(outer_triangle[1].y)}" stroke="black" fill="transparent" stroke-width="#{$stroke_width}"/>
-        <line x1="#{transform(inner_triangle[2].x)}" y1="#{transform(inner_triangle[2].y)}" x2="#{transform(outer_triangle[2].x)}" y2="#{transform(outer_triangle[2].y)}" stroke="black" fill="transparent" stroke-width="#{$stroke_width}"/>
+        <polygon points="#{inner_triangle_coordinates}"/>
+        <circle cx="#{transform(center.x)}" cy="#{transform(center.y)}" r="#{$pixels_per_unit.round}"/>
+        <line x1="#{transform(inner_triangle[0].x)}" y1="#{transform(inner_triangle[0].y)}" x2="#{transform(outer_triangle[0].x)}" y2="#{transform(outer_triangle[0].y)}"/>
+        <line x1="#{transform(inner_triangle[1].x)}" y1="#{transform(inner_triangle[1].y)}" x2="#{transform(outer_triangle[1].x)}" y2="#{transform(outer_triangle[1].y)}"/>
+        <line x1="#{transform(inner_triangle[2].x)}" y1="#{transform(inner_triangle[2].y)}" x2="#{transform(outer_triangle[2].x)}" y2="#{transform(outer_triangle[2].y)}"/>
     </symbol>
 
-    <use xlink:href="#logo" x="0" y="0" transform="rotate(20,#{transform(center.x)},#{transform(center.y)})" stroke="black" fill="transparent" stroke-width="#{$stroke_width}"/>
+    <use xlink:href="#logo" transform="rotate(20,#{transform(center.x)},#{transform(center.y)})" stroke="dodgerblue" fill="transparent" stroke-width="#{$stroke_width}"/>
 </svg>
 LOGO
